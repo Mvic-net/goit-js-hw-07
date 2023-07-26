43de948a82d5f7a718351e2c90b66cc5c13be541
Mvic-net <mvic.net@gmail.com>
2023-07-26 12:51:36 +0300

    bug-fix

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,14 +7,13 @@ const galleryContainer = document.querySelector(".gallery");
 // console.log(galleryContainer);
 const galleryMarkup = createGalleryEl(galleryItems);
 galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
-galleryContainer.addEventListener("click", openOriginalImg);
 
 function createGalleryEl(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
             <li class="gallery__item">
-            <a class="gallery__link" href="${original}">
+            <a class="gallery__link"href="${original}">
                 <img class="gallery__image" src="${preview}" alt="${description}" />
             </a>
             </li>
@@ -24,16 +23,8 @@ function createGalleryEl(galleryItems) {
     .join("");
 }
 
-function openOriginalImg(evt) {
-  evt.preventDefault();
-  var lightbox = new SimpleLightbox(".gallery a", {
-    captions: true,
-    captionsData: "alt",
-    captionDelay: 250,
-  });
-}
-// const lightbox = new SimpleLightbox(".gallery a", {
-//   captions: true,
-//   captionsData: "alt",
-//   captionDelay: 250,
-// });
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
